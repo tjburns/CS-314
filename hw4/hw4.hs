4fc87334c1437f4c3ad2b40b86c4c2f1a2b085ae
tjburns <tjburns28@gmail.com>
2019-04-19 01:41:32 -0400

    Big Boi Deletions
    Got rid of multiple extraneous folders that might actually be necessary
    if i want to make my final project within this repository.  It might end
    up being a project that I can turn into a personal project/resume
    builder so I might just do it as its own standalone repo.

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -1,1 +1,18 @@
 --TODO implement hangman in Haskell
+
+import System.Random
+
+
+getLine :: IO String 
+
+lines :: String -> [String]
+
+randomRIO :: Random a => (a, a) -> IO a
+
+readFile :: FilePath -> IO String
+
+words :: String -> [String]
+
+
+
+main = do

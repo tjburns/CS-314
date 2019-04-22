81167490b1c225fa201dfd5a689b4cc7d364860f
tjburns <tjburns28@gmail.com>
2019-04-22 02:43:24 -0400

    Completed hw4
    A simple hangman game in Haskell. Assumes proper input from the user.
    Chooses a random word from the associated "words.txt" file and then
    starts the game. Upon winning the user is presented with a victory
    string.  You cannot lose the game.

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -1,18 +1,64 @@
---TODO implement hangman in Haskell
 
+import System.IO
+import System.Environment
+import Data.List
 import System.Random
+import Control.Monad
+import Data.Char
+import Data.Typeable
 
 
-getLine :: IO String 
+pickWord :: [a] -> IO a
+pickWord xs = randomRIO (0, (length xs - 1)) >>= return . (xs !!)
 
-lines :: String -> [String]
+promptLine :: String -> IO String
+promptLine prompt = do
+	putStr prompt
+	getLine
 
-randomRIO :: Random a => (a, a) -> IO a
+gameLoop :: String -> String -> IO()
+gameLoop word guesses = do
+	let
+		wordWithGuesses = blankOrChar <$> word
+		blankOrChar c
+			| c `elem` guesses = c
+			| otherwise = '_'
+	--putStrLn "Word to guess: " ++ wordWithGuesses
+	--putStrLn "Guesses: " ++ guesses	
+	putStrLn wordWithGuesses
 
-readFile :: FilePath -> IO String
+	if word /= wordWithGuesses
+		then do
+			charGuess <- getLine
+			let newGuesses = guesses ++ charGuess
+			--recursive loop
+			gameLoop word newGuesses
+		else putStrLn "You won!"
 
-words :: String -> [String]
-
-
-
+main :: IO()
 main = do
+	-- assume first (and only) argument of hw4.hs is the wordlist used for the hangman game
+	--args <- getArgs
+
+	-- assume the file with words is understood to be in the working directory AND known by the program prior to running
+	contents <- readFile "words.txt"
+	let wordList = words contents
+	--let wordList = lines contents
+	--print wordList
+
+	word <- pickWord wordList
+	--putStrLn word
+	--putStrLn ("type of word is: " ++ (show (typeOf word)))	
+
+	--let wordGuess = map (\c -> '_') word
+	--putStrLn wordGuess
+
+	let guesses = ""
+	
+	-- enter gameloop with the chosen word and empty guesses string
+	gameLoop word guesses
+
+	-- game assumes guesses are input as lowercase characters.
+	-- proper input would be a single char
+	-- multiple guesses can be inputted as well as repeat guesses with the method used
+	-- upon winning a short victory string is printed and the program exits
